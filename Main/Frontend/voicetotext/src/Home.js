9bbff8f6da8e5a509e9f58ea36743810491ff44b
surya23 <surya.cherukuri@admitkard.com>
2020-02-05 21:15:57 -0700

    Made a grid layout for critegories

--- Main/Frontend/voicetotext/src/Home.js
+++ Main/Frontend/voicetotext/src/Home.js
@@ -1,21 +1,43 @@
 import React, { Component } from 'react';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-balham.css';
 import './index.css';
 
-
+  
 class Home extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            columnDefs: [
+                {headerName: 'Category', field: 'category'},
+                {headerName: 'Value', field: 'value'}
+            ],
+            rowData: [
+                {category: 'Identification', value: 'Celica'},
+                {category: 'Mechanism', value: 'Mondeo'},
+                {category: 'Injuries', value: 'Boxter'},
+                {category: 'Signs and Symptoms', value: 'Boxter'},
+                {category: 'Treatment', value: 'Boxter'},
+                {category: 'Allergies', value: 'Boxter'},
+                {category: 'Medications', value: 'Boxter'},
+                {category: 'Background', value: 'Boxter'},
+                {category: 'Other Info', value: 'Boxter'}
+            ]
+        }
+    }
 
     render() {
         return (
-            <div className="Home">
-               
-                <h1>
-                   Identificaion: Female 2years
-                   <br></br>
-                    Mechanism :
-
-
-
-                </h1>
+            <div
+                className="ag-theme-balham"
+                style={{ height: '300px', width: '420px', paddingLeft: '500px' }}
+            >
+                <AgGridReact
+                    columnDefs={this.state.columnDefs}
+                    rowData={this.state.rowData}>
+                </AgGridReact>
             </div>
         );
     }
