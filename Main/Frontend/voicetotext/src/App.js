16cf69764bed044b42de0e0671018220eb48744b
Yuti Desai <yrdesai@asu.edu>
2020-02-06 14:24:42 -0700

    Adding logo to the welcome page

--- Main/Frontend/voicetotext/src/App.js
+++ Main/Frontend/voicetotext/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Logo from “./pchlogo.png”;
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
 
@@ -7,7 +8,9 @@ class App extends Component {
   render() {
     return (
       <div className="App"> 
-		<Navbar />
+		<Navbar> 
+		
+		</Navbar>
 		<div className="App-bg" />
         <header className="App-header">
            <h1 className="App-title">Voice-To-Text Application of Pediatric IMIST-AMBO Criteria</h1>
