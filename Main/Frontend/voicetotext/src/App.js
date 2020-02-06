be62c31f141aa595eef71e36dbbdfce926ea1d7f
Yuti Desai <yrdesai@asu.edu>
2020-02-06 13:53:49 -0700

    Added navigation bar

--- Main/Frontend/voicetotext/src/App.js
+++ Main/Frontend/voicetotext/src/App.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from 'react-bootstrap/Navbar';
+import Navbar from './components/Navbar';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-	    <Navbar expand="lg" variant="light" bg="light">
-           <Navbar.Brand href="#">Navbar</Navbar.Brand>
-        </Navbar>
+      <div className="App"> 
+		<Navbar />
+		<div className="App-bg" />
         <header className="App-header">
            <h1 className="App-title">Voice-To-Text Application of Pediatric IMIST-AMBO Criteria</h1>
         </header>
---
+++ Main/Frontend/voicetotext/src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../App.css';
+
+function Navbar() {
+	
+	return(
+	<nav className="navbar sticky-top justify-content-end navbar-dark bg-dark">
+		<a class="navbar-brand" href="#">About</a>
+    </nav>
+	
+	);
+	
+	
+}
+
+export default Navbar;
