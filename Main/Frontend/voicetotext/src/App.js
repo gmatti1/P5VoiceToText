81a4488bfcb32b33f18064dfc7f2031d7cdc91cd
Yuti Desai <yrdesai@asu.edu>
2020-02-05 17:14:15 -0700

    US1Task27: Created Welcome page UI version1

--- Main/Frontend/voicetotext/src/App.js
+++ Main/Frontend/voicetotext/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,15 +6,12 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome</h1>
+          <h1 className="App-title">Voice-To-Text Application of Pediatric IMIST-AMBO Criteria</h1>
         </header>
-        <p className="App-intro">
-          SER517 Capstone Project - Team5 - Voice-to-text application of Pediatric IMIST-AMBO criteria
-        </p>
       </div>
     );
   }
 }
 
 export default App;
+
