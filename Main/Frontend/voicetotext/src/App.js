33015fabe4cf87e1464b5eff2c32ed61549d1354
Yuti Desai <yrdesai@asu.edu>
2020-02-05 18:59:36 -0700

    US1Task27 - Adding navbar

--- Main/Frontend/voicetotext/src/App.js
+++ Main/Frontend/voicetotext/src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from 'react-bootstrap/Navbar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+	    <Navbar expand="lg" variant="light" bg="light">
+           <Navbar.Brand href="#">Navbar</Navbar.Brand>
+        </Navbar>
         <header className="App-header">
-          <h1 className="App-title">Voice-To-Text Application of Pediatric IMIST-AMBO Criteria</h1>
+           <h1 className="App-title">Voice-To-Text Application of Pediatric IMIST-AMBO Criteria</h1>
         </header>
       </div>
     );
