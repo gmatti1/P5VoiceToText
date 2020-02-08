70179a6ebdc4a32e9d4d60f5fcca0af6008baf80
shefaliAJ7 <sanand22@asu.edu>
2020-02-08 13:52:40 -0700

    US02Task31: Updated code to check file extension

--- Main/Backend/Config.py
+++ Main/Backend/Config.py
@@ -1,7 +1,7 @@
 import os
 
 UPLOAD_FOLDER = os.path.abspath(os.path.dirname(__file__))+'/resources/VoiceUploads/'
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+ALLOWED_EXTENSIONS = {'txt', 'mp3'}
 
 DEV_IP = 'localhost'
 STG_IP = ''
