0057449bab044ddbedf1ee072d685b41a34bfd89
shashi <svanter1@asu.edu>
2020-02-13 00:50:52 -0700

    US3 task 36 modified alignmment added dummy text  of converted and categorized text

--- Main/Frontend/voicetotext/src/containers/ConvertedAndCategorizedText.js
+++ Main/Frontend/voicetotext/src/containers/ConvertedAndCategorizedText.js
@@ -5,8 +5,21 @@ class ConvertedAndCategorizedText extends Component {
   render() {
     return (
       <div className='wrapper'>
-        <div className='convertedText'>Converted Text</div>
-        <div className='categorizedText'>Categorized Text</div>
+        <div className='convertedText'>Converted Text
+          <div style={{fontSize: '0.5em', textAlign: 'left'}}>Lorem ipsum dolor sit amet, ius an error scripta. No quidam omnesque sea, habemus moderatius eos ad. No his consul admodum concludaturque, saperet indoctum mel an, has laoreet suscipiantur te. Cibo falli debitis pro no, sed minim novum eu, id stet postea meliore eos. Te vis wisi decore, in duo corpora vivendum.
+            Errem assueverit mei ei. Omnium tamquam per ex, mutat salutatus ei est. Elit lucilius ad mel, detracto pertinax scriptorem te sed. Cu eros mnesarchum voluptatum eos, nec eu fugit choro, assum dolor ex nec. Wisi paulo persecuti ex ius, eum et sint aperiri partiendo.
+        </div>
+        </div>
+        <div className='categorizedText'>Categorized Text
+        <div style={{fontSize: '0.5em', textAlign: 'left'}}>
+                  Info:  6 y/o female​<br/>
+                  Mechanism:  GSW​<br/>
+                  Injuries:  Evisceration of bowel​<br/>
+                  Signs (VS, GCS):  Unable to obtain BP/HR 166 = shock! GCS 3 (is important (especially in blunt trauma))​<br/>
+                  Treatment and Trends:  RSI, on Vent (100% Saturation)​<br/>
+                  Allergies, Meds, Background, Other – [often all negative in children]
+        </div>
+        </div>
       </div>
     );
   }
