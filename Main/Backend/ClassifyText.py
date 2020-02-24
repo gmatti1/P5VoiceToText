ba57e68d6779f4184462bfa0f5d6c51ec5bd52dd
surya23 <surya.cherukuri@admitkard.com>
2020-02-23 21:43:45 -0700

    Added stopwords code in classifytext file

--- Main/Backend/ClassifyText.py
+++ Main/Backend/ClassifyText.py
@@ -1,3 +1,9 @@
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize 
+from nltk.stem import PorterStemmer
+from string import punctuation
+
 class ClassifyText:
 
 	def __init__(self):
@@ -5,6 +11,22 @@ class ClassifyText:
 
 	def remove_stopwords(self):
 		print("Removing Stop Words ....")
+		example_sent = "This is a sample sentence, showing off the stop words filtration."
+  
+		stop_words = set(stopwords.words('english') + list(punctuation)) 
+		
+		word_tokens = word_tokenize(example_sent) 
+		
+		filtered_sentence = [w for w in word_tokens if not w in stop_words] 
+		
+		filtered_sentence = [] 
+		
+		for w in word_tokens: 
+			if w not in stop_words: 
+				filtered_sentence.append(w) 
+  
+		print(word_tokens) 
+		print(filtered_sentence) 
 
 	def stemming_text(self):
 		print("Get the root of words using Stemming ....")
