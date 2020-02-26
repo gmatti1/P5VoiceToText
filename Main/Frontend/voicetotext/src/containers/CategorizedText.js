7320270ace14fb7f8b3be741033c612ea94a7688
shefaliAJ7 <sanand22@asu.edu>
2020-02-25 17:40:02 -0700

    US41Task65: Response from Categorized Text API is visible in UI now

--- Main/Frontend/voicetotext/src/containers/CategorizedText.js
+++ Main/Frontend/voicetotext/src/containers/CategorizedText.js
@@ -12,15 +12,24 @@ class CategorizedText extends Component {
 	
 	this.state = {
 		
-	     Identification:'CelicaCelicaCelicaCelicaCelicaCelicaCelicaCelicaCelicaCelicaCelicaCelicaCelica' ,
-         Mechanism: 'Mondeo' ,
-         Injuries: 'Boxter' ,
-         Symptoms: 'Boxter' ,
-         Treatment: 'Boxter' ,
-         Allergies: 'Boxter' ,
-         Medications: 'Boxter' ,
-         Background: 'Boxter' ,
-         Other: 'Boxter'
+	     identification:'CelicaCelicaCelicaCelicaCelicaCelicaCelicaCelicaCelicaCelicaCelicaCelicaCelica' ,
+         mechanism: 'Mondeo' ,
+         injury: 'Boxter' ,
+         signs: 'Boxter' ,
+         treatment: 'Boxter' ,
+         allergy: 'Boxter' ,
+         medication: 'Boxter' ,
+         background: 'Boxter' ,
+         other: 'Boxter',
+         textCategorized:{ "identification" : [],
+                  "mechanism" : [],
+                  "injury": [],
+                  "signs": [],
+                  "treatment": [],
+                  "allergy": [],
+                  "medication": [],
+                  "background": [],
+                  "other": [] }
 		};
 
   }
@@ -29,12 +38,66 @@ class CategorizedText extends Component {
     fetch('/categorizeText').then(response =>
       response.json().then(data => {
         this.setState({ textCategorized: data });
-        console.log(this.state.textCategorized);
-      })
+        console.log(this.state.textCategorized);      })
     );
   }
 
   render() {
+    var identification_keywords = "";
+    if(this.state.textCategorized.identification.length >= 1)
+      identification_keywords = this.state.textCategorized.identification[0];
+    for(var i = 1; i< this.state.textCategorized.identification.length; i++)
+      identification_keywords+= ', ' + this.state.textCategorized.identification[i];
+
+    var mechanism_keywords = "";
+    if(this.state.textCategorized.mechanism.length >= 1)
+      mechanism_keywords = this.state.textCategorized.mechanism[0];
+    for(var i = 1; i< this.state.textCategorized.mechanism.length; i++)
+      mechanism_keywords+= ', ' + this.state.textCategorized.mechanism[i];
+
+    var injury_keywords = "";
+    if(this.state.textCategorized.injury.length >= 1)
+      injury_keywords = this.state.textCategorized.injury[0];
+    for(var i = 1; i< this.state.textCategorized.injury.length; i++)
+      injury_keywords+= ', ' + this.state.textCategorized.injury[i];
+
+    var signs_keywords = "";
+    if(this.state.textCategorized.signs.length >= 1)
+      signs_keywords = this.state.textCategorized.signs[0];
+    for(var i = 1; i< this.state.textCategorized.signs.length; i++)
+      signs_keywords+= ', ' + this.state.textCategorized.signs[i];
+
+    var treatment_keywords = "";
+    if(this.state.textCategorized.treatment.length >= 1)
+      treatment_keywords = this.state.textCategorized.treatment[0];
+    for(var i = 1; i< this.state.textCategorized.treatment.length; i++)
+      treatment_keywords+= ', ' + this.state.textCategorized.treatment[i];
+
+    var allergy_keywords = "";
+    if(this.state.textCategorized.allergy.length >= 1)
+      allergy_keywords = this.state.textCategorized.allergy[0];
+    for(var i = 1; i< this.state.textCategorized.allergy.length; i++)
+      allergy_keywords+= ', ' + this.state.textCategorized.allergy[i];
+
+    var medication_keywords = "";
+    if(this.state.textCategorized.medication.length >= 1)
+      medication_keywords = this.state.textCategorized.medication[0];
+    for(var i = 1; i< this.state.textCategorized.medication.length; i++)
+      medication_keywords+= ', ' + this.state.textCategorized.medication[i];
+
+    var background_keywords = "";
+    if(this.state.textCategorized.background.length >= 1)
+      background_keywords = this.state.textCategorized.background[0];
+    for(var i = 1; i< this.state.textCategorized.background.length; i++)
+      background_keywords+= ', ' + this.state.textCategorized.background[i];
+
+    var other_keywords = "";
+    if(this.state.textCategorized.other.length >= 1)
+      other_keywords = this.state.textCategorized.other[0];
+    for(var i = 1; i< this.state.textCategorized.other.length; i++)
+      other_keywords+= ', ' + this.state.textCategorized.other[i];    
+
+
     return (
     
    <div className ='Categorydata'> 
@@ -52,39 +115,39 @@ class CategorizedText extends Component {
   <tbody >
     <tr>
       <td>Identification</td>
-      <td>{this.state.Identification}</td>
+      <td>{identification_keywords}</td>
     </tr>
     <tr>
       <td>Mechanism</td>
-      <td>{this.state.Mechanism}</td>
+      <td>{mechanism_keywords}</td>
     </tr>
     <tr>
       <td>Injuries</td>
-      <td>{this.state.Injuries}</td>
+      <td>{injury_keywords}</td>
     </tr>
 	<tr>
-      <td>Symptoms</td>
-      <td colSpan="2">{this.state.Symptoms}</td>
+      <td>Signs</td>
+      <td colSpan="2">{signs_keywords}</td>
     </tr>
 	<tr>
       <td>Treatment</td>
-      <td>{this.state.Treatment}</td>
+      <td>{treatment_keywords}</td>
     </tr>
 	<tr>
       <td>Allergies</td>
-      <td>{this.state.Allergies}</td>
+      <td>{allergy_keywords}</td>
     </tr>
 	<tr>
       <td>Medications</td>
-      <td>{this.state.Medications}</td>
+      <td>{medication_keywords}</td>
     </tr>
 	<tr>
       <td>Background</td>
-      <td>{this.state.Background}</td>
+      <td>{background_keywords}</td>
     </tr>
 	<tr>
       <td>Other Info</td>
-      <td>{this.state.Other}</td>
+      <td>{other_keywords}</td>
     </tr>
   </tbody>
 </Table>
