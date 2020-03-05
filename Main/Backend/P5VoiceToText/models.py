86c3246c6c9a17ffd490ffe3532c6bca644224cf
shashi <svanter1@asu.edu>
2020-03-05 00:22:45 -0700

    us74 task79 created a database schema to store filename, s3link, categorized text, converted text

--- Main/Backend/P5VoiceToText/models.py
+++ Main/Backend/P5VoiceToText/models.py
@@ -3,4 +3,10 @@ from P5VoiceToText import db
 class Imist_ambo_template(db.Document):
 	keyword = db.StringField(required=True, unique=True)
 	category = db.StringField(required=True)
+	
+class ConvertedText(db.Document):
+    file_name = db.StringField(required=True, unique=True)
+    s3_link = db.StringField(required=True)
+    convertedText = db.StringField(required=True)
+    categorizedText=db.StringField( required=True)
 
